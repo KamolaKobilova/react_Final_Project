3994201680b9bf92272c641cef3d201bea993430
“Kamola <kamola_8880@mail.ru>
2022-02-28 23:58:30 +0500

    style header

--- src/components/Header_2/Header_2.jsx
+++ src/components/Header_2/Header_2.jsx
@@ -2,29 +2,31 @@ import React from 'react';
 import {Autocomplete} from '@react-google-maps/api';
 import {AppBar, Toolbar, Typography, InputBase, Box} from '@material-ui/core';
 import { CallMissedSharp } from '@material-ui/icons';
-import SearchIcon from '@material-ui/icons';
-import { Classes } from '@material-ui/styles/mergeClasses/mergeClasses';
+import SearchIcon from '@material-ui/icons/Search';
+import useStyle from './style'
+
 
 
 function Header_2() {
+   const classes = useStyle()
   return (
     <AppBar position='static'>
         <Toolbar className={classes.toolbar}>
           <Typography variant='h5' className={classes.title}>
               Travel  advisor
           </Typography>
-          <Box display={flex}>
+          <Box display="flex">
           <Typography variant='h6' className={classes.title}>
               Explore new places
           </Typography> 
-          <Autocomplete>
+          {/* <Autocomplete> */}
               <div className={classes.search}>
                   <div className={classes.searchIcon}>
                      <SearchIcon/>
                   </div>
                   <InputBase placeholder='Search...' classes={{root: classes.inputRoot, input: classes.inputInput}}/>
               </div>
-          </Autocomplete>
+          {/* </Autocomplete> */}
           </Box>
         </Toolbar>
 
