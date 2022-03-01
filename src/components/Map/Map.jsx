540ea6ee1e0aaab7e7155b6ec65b8b11a84ac39e
“Kamola <kamola_8880@mail.ru>
2022-03-01 22:17:54 +0500

    google map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+///
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import {Paper, Typography, useMediaQuery} from '@material-ui/core';
+import LocationOnOutlinedIcon  from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './style'
+
+
 
 function Map() {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('min-width: 600px ');
+  const coordinate = {lat: 0, lng:0 }
+
   return (
-    <div>Map</div>
+     <div className={classes.mapContainer}>
+        <GoogleMapReact 
+        bootstrapURLKeys={{ key: 'AIzaSyCPeLAkle0tIULImvEkStR8xj0pLnEQ5a0'}}
+        defaultCenter={coordinate}
+        center={coordinate}
+        defaultZoom={14}
+        margin={[50,50,50,50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+        >
+
+          
+        </GoogleMapReact>
+     </div>
   )
-}
+} 
 
 export default Map
