7fbb23de6d564d72d72d482527e91af830597695
“Kamola <kamola_8880@mail.ru>
2022-03-08 23:23:47 +0500

    content

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 import {Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip} from '@material-ui/core ';
 import LocationOnIcon from '@material-ui/icon/LocationOn';
 import  PhoneIcon from '@material-ui/icons/Phone';
-import Rating from '@material-ui/lab/Rating'
+import Rating from '@material-ui/lab/Rating';
+
+import useStyles from './style'
+ 
 function PlaceDetails({place }) {
-  console.log(place)
+ const classes = useStyles( )
   return (
-    <div>{place.name}</div>
+    <Card elevation={6}>
+        <CardMedia
+            style={{height: 350 }}
+            image={place.photo? place.photo.images.large.url: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fphotos.smugmug.com%2F3702%2FFood-Photography%2Fi-9tz2Btd%2F0%2F4d218668%2FL%2FEclectica--20-L.jpg&imgrefurl=https%3A%2F%2Fwww.myiconography.com%2FFoodPhotography&tbnid=FzCBzGipwdc-gM&vet=12ahUKEwiM6c7LkLf2AhWL0-AKHTbADbQQMygNegUIARD3AQ..i&docid=PpDKZBkYZCOIzM&w=800&h=534&q=food%20photography&hl=en-GB&ved=2ahUKEwiM6c7LkLf2AhWL0-AKHTbADbQQMygNegUIARD3AQ'}
+            title={place.name }
+        />
+        <CardContent>
+           <Typography gutterBottom variant="h5">{place.name}</Typography>
+        <CardContent/>
+    </Card>
   )
 }
 
